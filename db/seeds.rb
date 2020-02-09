05f44d784de93ec90c4dca73af568e7fed2a8a97
EC2 Default User <ec2-user@ip-172-31-18-32.us-west-2.compute.internal>
2020-02-09 23:20:34 +0000

    B8 0210

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,13 @@
 # coding: utf-8
 
-User.create!( name: "Sample User",
+User.create!( name: "管理者",
               email: "sample@email.com",
               password: "password",
+              password_confirmation: "password",
+              admin: true)
+           
+User.create!( name: "中山　航",
+              email: "nakayama@email.com",
+              password: "password",
               password_confirmation: "password")
+               
